22b08a4baff62186f375304e6e3ab1c95642886f
Daniela Capponi <capponi.daniela@gmail.com>
2020-02-03 09:06:52 -0300

    fix expression

--- tarea/js/pagoCredito.js
+++ tarea/js/pagoCredito.js
@@ -6,6 +6,8 @@ const creditCard = confirm("Desea pagarla con tarjeta?");
 
 let installements = Number(creditCard && prompt("Elija la cantidad de cuotas, 3, 6 u 12?"));
 
-totalAmount = (installements == 3 && (ammount + (ammount * 5)/100)) ||  (installements == 6 && (ammount + (ammount * 10)/100)) ||  (installements == 12 && (ammount + (ammount * 15)/100));
+totalAmount = ((installements == 3 && (ammount + ((ammount * 5) / 100))) || (installements == 6 && (ammount + ((ammount * 10) / 100))) || (installements == 12 && (ammount + ((ammount * 15) / 100))));
 
-creditCard && alert(`El monto total a pagar es de ${totalAmount}. Usted eligió pagar en ${installements} cuotas de ${totalAmount / installements}`);
+creditCard && alert(`El monto original es de ${ammount} pesos.
+Usted eligió pagar en ${installements} cuotas de ${totalAmount / installements} pesos.
+El monto total a pagar es de ${totalAmount} pesos.`);
