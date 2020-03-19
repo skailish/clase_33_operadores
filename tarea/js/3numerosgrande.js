1405afc8f5e367f3b11d1c23253ccdffe816b580
Daniela Capponi <capponi.daniela@gmail.com>
2020-03-19 08:53:44 -0300

    update

--- tarea/js/3numerosgrande.js
+++ tarea/js/3numerosgrande.js
@@ -2,4 +2,7 @@ const value1 = Number(prompt("Ingrese un número"));
 const value2 = Number(prompt("Ingrese otro número"));
 const value3 = Number(prompt("Ingrese un tercer número"));
 
-value1 > value2 && value1 > value3 && alert(`${value1} es el mayor`) || value2 > value1 && value2 > value3 && alert(`${value2} es el mayor`) || value3 > value1 && value3 > value2 && alert(`${value3} es el mayor`) || (value1 == value2 || value2 == value3 || value3 == value1) && alert("Hay números con el mismo valor");
+value1 > value2 && value1 > value3 && alert(`${value1} es el mayor`) ||
+    value2 > value1 && value2 > value3 && alert(`${value2} es el mayor`) ||
+    value3 > value1 && value3 > value2 && alert(`${value3} es el mayor`) ||
+    (value1 == value2 || value2 == value3 || value3 == value1) && alert("Hay números con el mismo valor");
